4acce4c8f19e498a9e7bdfca2bef727d83d18572
hudyu17 <hengjeung.yuen@gmail.com>
2023-04-12 23:36:35 -0400

    cleveland income

---
+++ model/data/walkscore.py
@@ -0,0 +1,4 @@
+from walkscore import WalkScoreAPI
+
+# using an API key in the "WALKSCORE_API_KEY" environment variables
+walkscore = WalkScoreAPI()
---
+++ model/src/data/make_dataset.py
@@ -0,0 +1,3 @@
+# if __name__ == "__main__":
+# take user input for year, select brt system
+# throw data into somewhere?
